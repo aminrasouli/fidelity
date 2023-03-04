2344a15a7aaddf95ad12bff6302e04b292f2aa07
Amin Rasouli <ma.rasouli@digikala.com>
2023-03-05 00:21:13 +0330

    add smooth scroll css

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,8 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+import 'src/styles/index.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
